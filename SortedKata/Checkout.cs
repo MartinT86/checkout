0eee5e7ee00862dc45108d8152a4585fb318cb1f
Martin Tierney <martin.tierney86@hotmail.co.uk>
2020-03-29 15:53:01 +0100

    Passing for single scan test

--- SortedKata/Checkout.cs
+++ SortedKata/Checkout.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SortedKata
 {
     public class Checkout
     {
+        private readonly IList<Item> items;
+
+        public Checkout()
+        {
+            items = new List<Item>();
+        }
+
         public decimal Total()
         {
+            if (items.Count > 0)
+            {
+                return 0.5m;
+            }
             return 0;
         }
 
         public void Scan(Item item)
         {
-            throw new NotImplementedException();
+            items.Add(item);
         }
     }
 }
