eed1f18a7c37f00b5c633df578a49737f54c22c7
Martin Tierney <martin.tierney86@hotmail.co.uk>
2020-03-29 16:43:35 +0100

    Failing test for A99 promotion

---
+++ SortedKata.Tests/PromotionTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortedKata.Tests
+{
+    [TestFixture]
+    public class PromotionTests
+    {
+        [Test]
+        public void ThreeA99_Returns130()
+        {
+            const decimal expectedTotal = 1.30m;
+            var priceReader = new MemoryPriceReader();
+            var checkout = new Checkout(priceReader);
+            var item = new Item("A99");
+
+            checkout.Scan(item);
+            checkout.Scan(item);
+            checkout.Scan(item);
+            var total = checkout.Total();
+
+            Assert.AreEqual(expectedTotal, total);
+        }
+    }
+}
